a20bba8e200fcf4ccb3c52e54553c878a58b1627
Tkacz <gabrielmt2@al.insper.edu.br>
2022-02-06 21:48:06 -0300

    init

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+from random import randint
+
+def main():
+    n = 1
+    
+    while True:
+        os.system('cls')
+        print(f'Current n: {n}')
+        print(f'Current odds: {1/(2**n)}')
+        print(f'Current value: {2**n}')
+        
+        p = randint(1, 10)
+        if p <= 5:
+            print('Won.')
+            n+=1
+        else:
+            print('Lost.')
+            break
+
+if __name__ == '__main__':
+    main()
