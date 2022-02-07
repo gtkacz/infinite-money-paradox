fd2a54e270bdc870eb1aa1dbca55bdc6104f77c5
Tkacz <gabrielmt2@al.insper.edu.br>
2022-02-06 21:54:52 -0300

    att

--- main.py
+++ main.py
@@ -2,17 +2,18 @@ import os
 from random import randint
 
 def main():
+    os.system('cls')
     n = 1
     
     while True:
-        os.system('cls')
+        #os.system('cls')
         print(f'Current n: {n}')
         print(f'Current odds: {1/(2**n)}')
         print(f'Current value: {2**n}')
         
         p = randint(1, 10)
         if p <= 5:
-            print('Won.')
+            print('Won.\n')
             n+=1
         else:
             print('Lost.')
